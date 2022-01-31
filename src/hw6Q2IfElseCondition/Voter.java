5ca197572a989c1b26ff2d7aed6a74c5a5eaf8de
showkot akbor <showkotakbor@showkots-MBP.fios-router.home>
2022-01-31 10:17:10 -0500

    01312022 10.17am

--- src/hw6Q2IfElseCondition/Voter.java
+++ src/hw6Q2IfElseCondition/Voter.java
@@ -16,4 +16,3 @@ public class Voter {
 }
 }
 
-//Now write some condition. a) your age is equal to 18, outcome will be "I am a Voter" b) your age is less than 18, outcome will be "I am not a Voter" c) your age is greater than 18, outcome will be "I am a Voter" d) And finally the outcome will be "Please add a valid age". In the above 4 condition, use appropriate key word like if, else if, else to execute that you are a voter or not. Please organize the code (very important). don't give any white space. push the code to GitHub and add the link below.
