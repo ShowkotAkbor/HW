d51a11b59cf29b9ea43439f45fdc0d97d74bd024
showkot akbor <showkotakbor@showkots-MBP.fios-router.home>
2022-01-31 10:13:58 -0500

    01312022 10.13am

--- src/hw6Q2IfElseCondition/Voter.java
+++ src/hw6Q2IfElseCondition/Voter.java
@@ -6,15 +6,14 @@ public class Voter {
 			System.out.println("I am a Voter");
 		} else {
 			System.out.println("I am not a Voter");
-		}
-		if (age < 18) {
+		} if (age < 18) {
 			System.out.println("I am not a voter");
-		} else if (age > 18) {
+		} else if (age>18) {
 			System.out.println("I am a Voter");
 		} else {
 			System.out.println("Please add a valid age");
 		}
-	}
+}
 }
 
 //Now write some condition. a) your age is equal to 18, outcome will be "I am a Voter" b) your age is less than 18, outcome will be "I am not a Voter" c) your age is greater than 18, outcome will be "I am a Voter" d) And finally the outcome will be "Please add a valid age". In the above 4 condition, use appropriate key word like if, else if, else to execute that you are a voter or not. Please organize the code (very important). don't give any white space. push the code to GitHub and add the link below.
--- src/hw7Q2UseOfNestedIf/DiabeticCondition.java
+++ src/hw7Q2UseOfNestedIf/DiabeticCondition.java
@@ -2,19 +2,19 @@ package hw7Q2UseOfNestedIf;
 import java.util.Scanner;
 public class DiabeticCondition {
 	public static void main(String[] args) {
-		System.out.println("please enter your Hemoglobin A1c value bellow");
+		System.out.println("<please enter your Hemoglobin A1c value bellow>");
 		Scanner scanner = new Scanner(System.in);
 		double hbga1c = scanner.nextDouble();
 		if (hbga1c > 6.4) {
-			System.out.println("I am a diabetic patient");
-		} else { 
-			System.out.println("Congratulation! you are not diabetic");
-			if (hbga1c >= 5.7)
-				if (hbga1c < 5.7)
-					System.err.println("I am a pre-diabetic patient");
-				else {
-					System.out.println("I am a healthy person");
-		}
-			     }
+		System.out.println("I am a diabetic patient");
+		} else {
+		if (hbga1c >=5.7)
+			System.out.println("I am a pre-diabetic patient");
+		if (hbga1c <5.7)
+		System.out.println("I am a healthy person");
+				}
+                
+		 scanner.close();
+
 }
 }
